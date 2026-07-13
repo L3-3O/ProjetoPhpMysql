b1942da16295c3f2a4fdcda07210ee427f82a832
L3-3O <leonatan2500@gmail.com>
2026-07-13 15:58:14 -0300

    L13/07

---
+++ admin/componentes/conexao.php
@@ -0,0 +1,25 @@
+<?php
+require_once'config.php';
+$host ="localhost";
+$banco="estoque";
+$usuario="root";
+$senha="";
+
+try{
+    $pdo=new PDO(
+        "mysqlhost=$host;dbname=$banco;chartset=utf8",
+        $usuario,
+        $senha
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    )
+} catch(PDOException $erro){
+    die("Erro na conexão".
+    $erro->getMessage())
+    
+;}
+?>
+
---
+++ admin/componentes/header.php
@@ -0,0 +1,74 @@
+<header class="topbar">
+            
+            
+
+            <button class="btn btn-light" id="menuButton">
+
+                <i class="bi bi-list"></i>
+
+            </button>
+
+            <div class="search">
+
+                <input type="text"
+                    class="form-control"
+                    placeholder="Pesquisar...">
+
+            </div>
+
+            <div class="header-actions">
+
+                <button class="btn btn-light position-relative">
+
+                    <i class="bi bi-bell"></i>
+
+                    <span
+                        class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        8
+                    </span>
+
+                </button>
+
+                <button class="btn btn-light">
+
+                    <i class="bi bi-chat-dots"></i>
+
+                </button>
+
+                <button class="btn btn-light"
+                    id="themeButton">
+
+                    <i class="bi bi-moon-stars"></i>
+
+                </button>
+
+                <div class="dropdown">
+
+                    <button
+                        class="btn btn-light dropdown-toggle"
+                        data-bs-toggle="dropdown">
+
+                        <img src="https://i.pravatar.cc/40?img=15"
+                            class="rounded-circle">
+
+                        João Silva
+
+                    </button>
+
+                    <ul class="dropdown-menu dropdown-menu-end">
+
+                        <li><a class="dropdown-item" href="#">Meu Perfil</a></li>
+
+                        <li><a class="dropdown-item" href="#">Configurações</a></li>
+
+                        <li><hr class="dropdown-divider"></li>
+
+                        <li><a class="dropdown-item text-danger" href="#">Sair</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </header>
